4918f3befffaa73468cdf9c609ae8c7eeb970ecf
Manu G <manuelgonzalezcastro1998@gmail.com>
2019-02-20 10:29:39 +0100

    1 devuelve 1

---
+++ test/com/cifpcarballeira/katas/FizzBuzz/Tests/TestFizzBuzz.java
@@ -0,0 +1,25 @@
+package com.cifpcarballeira.katas.FizzBuzz.Tests;
+
+// MGC
+import com.cifpcarballeira.katas.FizzBuzz.FizzBuzz;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author daw111
+ */
+public class TestFizzBuzz {
+    
+    public TestFizzBuzz() {
+    }
+    
+    @Test
+    public void test1devuelve1(){
+        FizzBuzz fb = new FizzBuzz();
+        String result = fb.getFizzBuzz(1);
+        assertEquals("1", result);
+    }
+
+
+}
